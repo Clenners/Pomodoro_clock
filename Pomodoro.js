f6eb0ab743dbd08a41700e39803a5eb2e6cb25bb
Zach <zach.clenaghan@gmail.com>
2015-11-08 21:47:43 +0000

    made the timer actually work
    it ticks down correctly. need to add an alert when it reaches zero and
    also an interval timer etc.

--- Pomodoro.js
+++ Pomodoro.js
@@ -5,13 +5,105 @@ document.getElementById("start-time").innerHTML = startTime;
 document.getElementById("int-length").innerHTML = intLength;
 document.getElementById("timer").innerHTML = startTime;
 
+var decreaseIntByOne = function() {
+    var numbifyArr = intLength.split(":");
+    numbifyArr[0] = parseInt(numbifyArr[0]);
+
+    if (numbifyArr[0] === 0) {
+      return;
+    }
+
+    numbifyArr[0] = numbifyArr[0] - 1;
+    intLength = numbifyArr.join(":");
+
+    document.getElementById("int-length").innerHTML = intLength;
+};
 
 
-/* This is Code from a youtube tutorial showing how to make a timer
+var increaseIntByOne = function() {
+    var numbifyArr = intLength.split(":");
+    numbifyArr[0] = parseInt(numbifyArr[0]);
 
-function countDown() {
-    this.start_time = "25:00";
+    numbifyArr[0] = numbifyArr[0] + 1;
+    intLength = numbifyArr.join(":");
+
+    document.getElementById("int-length").innerHTML = intLength;
+};
+
+
+var decreaseStartByOne = function() {
+    var numbifyArr = startTime.split(":");
+    numbifyArr[0] = parseInt(numbifyArr[0]);
+
+    if (numbifyArr[0] === 0) {
+      return; // I'm hoping this will stop the whole function if true.... IT DOES WOOHOO!
+    }
+
+    numbifyArr[0] = numbifyArr[0] - 1;
+    startTime = numbifyArr.join(":");
+
+    document.getElementById("start-time").innerHTML = startTime;
+    document.getElementById("timer").innerHTML = startTime;
+};
+
+
+var increaseStartByOne = function() {
+    var numbifyArr = startTime.split(":");
+    numbifyArr[0] = parseInt(numbifyArr[0]);
+
+    numbifyArr[0] = numbifyArr[0] + 1;
+    startTime = numbifyArr.join(":");
+
+    document.getElementById("start-time").innerHTML = startTime;
+    document.getElementById("timer").innerHTML = startTime;
+};
+
+
+var initiateTimer = function() {
+    var numbifyArr = startTime.split(":");
+    numbifyArr[0] = parseInt(numbifyArr[0]);
+    numbifyArr[1] = parseInt(numbifyArr[1]);
+
+    if (numbifyArr[1] === 0) {
+        numbifyArr[1] = 59;
+        numbifyArr[0] = numbifyArr[0] - 1;
+    }
+    else {
+        numbifyArr[1] = numbifyArr[1] - 1;
+    }
+
+    if (numbifyArr[1] < 10) {
+        numbifyArr[1] = "0" + numbifyArr[1];
+    }
+
+    startTime = numbifyArr.join(":");
+    document.getElementById("timer").innerHTML = startTime;
+};
+
+      /*if (numbifyArr[1] > 0 || numbifyArr[0] > 0) {
+        if (numbifyArr[1] == 0) {
+            numbifyArr[0] = numbifyArr[0] - 1;
+            numbifyArr[1] = 59;
+        }
+        else {numbifyArr[1] = numbifyArr[1] - 1;
+        }
+      }
+
+      seconds = numbifyArr[1];
+      if (numbifyArr[1] < 10) {
+          seconds = "0" + seconds;
+          document.getElementById("timer").innerHTML = numbifyArr[0] + ":" + seconds;
+          }
+}*/
+
+//var timerGo = setInterval(initiateTimer, 1000);
+
+//This is Code from a youtube tutorial showing how to make a timer
+
+/*function initiateTimer() {
+    this.start_time = startTime;
     this.target_id = "#timer";
+    this.name = "timer";
 }
 
 countDown.prototype.init = function() {
@@ -43,4 +135,4 @@ Countdown.prototype.update_target = function() {
         seconds = "0" + seconds;
         $(this.target_id).val(this.minutes + ":" + seconds);
     }
-} */
+}*/
